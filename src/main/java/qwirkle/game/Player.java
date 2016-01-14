882f3f364eca36c2a2940ea5c950d5d3e9cc5fe0
Mathay Kahraman <g.m.kahraman@student.utwente.nl>
2016-01-14 13:11:38 +0100

    Add Player & HumanPlayer first versions

--- src/main/java/qwirkle/game/Player.java
+++ src/main/java/qwirkle/game/Player.java
@@ -9,39 +9,60 @@ public class Player {
     private int score;
     private Set<Block> hand;
 
-    //Constructor zet de naam op een nieuwe naam, geeft een nieuwe lege hand mee en zet de score op 0.
+    /**
+     * Constructs a new player, which sets the name, a new hand and sets the score on 0.
+     * @param name
+     */
     public Player(String name) {
         setName(name);
         hand = new HashSet<>();
         score = 0;
     }
 
-    //1 block neerleggen.
+    /**
+     * Puts 1 block on the board and removes the block out of your hand.
+     * @param block
+     */
     public void addBlock2(Block block) { //Tom IntelliJ zeurt er over dat het 2 methods zijn met de zelfde naam, fix het.
         hand.remove(block); //is het remove of add? ik zou denken remove, want je haalt ze uit je hand op het board.
     }
 
-    //Meerdere blokken tegelijkertijd zetten.
+    /**
+     * Puts multiple blocks on the board and removes the blocks out of your hand.
+     * @param blockSet
+     */
     public void addBlock(HashSet<Block> blockSet) {
         hand.remove(blockSet); //kan dit zo of moet dit met een loop? Ik denk zelf wel met een loop.
     }
 
-    //Verwijder een blok die je hebt neergelegd.
+    /**
+     * Removes a block from the board, which you just putted on the board.
+     * @param block
+     */
     public void removeBlock(Block block) {
         hand.add(block); //hier dus weer dezelfde vraag, hierm oet add denk ik, want je pakt ze van het board.
     }
 
-    //Geef de speler een naam.
+    /**
+     * Sets the name of an player.
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
-    //Vraag de naam van de speler op.
+    /**
+     * Returns the name of the player.
+     * @return the name of the player.
+     */
     public String getName() {
         return this.name;
     }
 
-    //Kun je de score mee verhogen.
+    /**
+     * Edits the score with the amount score.
+     * @param score
+     */
     public void addScore(int score) {
         this.score = this.score + score;
     }
